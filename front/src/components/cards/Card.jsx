21955f32fd4b961053d698b49991e5000f41b1b0
fabiorodrigolimamartins <fabiorodrigolima@gmail.com>
2022-04-26 23:43:31 -0300

    Alterado o componente card, para deixar melhor encapsulada as funções no pai.

--- front/src/components/cards/Card.jsx
+++ front/src/components/cards/Card.jsx
@@ -3,28 +3,20 @@ import instance from "../../config/instance";
 import deleteIcon from '../../icons/crud/delete.svg'
 
 
-export default function Card(items,) {
-  const trainerId = "ce771e34-c3f6-4794-b4cf-167b53144561"
-  async function handleDelete(id) {
-    const response = await instance.delete(`/trainers/${trainerId}/pokemons/${id}`)
-  }
-  
-  return items.items.length > 0 ? (
+export default function Card(props) {
+  console.log(props)
+  return (
     <div className="flex flex-wrap bg-white gap-4">
-      {items.items.map((item, index) => (
-        <div className="w-80 h-40 border-2 px-4 rounded-xl" key={`${index}`}>
-          <label htmlFor={index}>Pokemon:</label>
-          <h4 className="uppercase tracking-wide text-sm text-indigo-500 font-semibold" id={index}>
-            {item.pokemon?.name}
+        <div className="w-80 h-40 border-2 px-4 rounded-xl">
+          <label htmlFor={props.id}>Pokemon:</label>
+          <h4 className="uppercase tracking-wide text-sm text-indigo-500 font-semibold" id={props.item.id}>
+            {props.item.pokemon?.name}
           </h4>
-          <p>Level: {item.level}</p>
+          <p>Level: {props.item.level}</p>
           <span className="w-10 h-10 align-bottom">
-            <img src={deleteIcon} onClick={(e) => handleDelete(item.id)} alt="" />
+            <img src={deleteIcon} onClick={(e) => props.handleDelete(props.item.id)} alt="" />
           </span>
         </div>
-      ))}
     </div>
-  ) : (
-    <h4 className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Não há nenhum Pokemon cadastrado.</h4>
-  );
+  )
 }
--- front/src/pages/Pokemon.jsx
+++ front/src/pages/Pokemon.jsx
@@ -1,58 +1,127 @@
-import React, { useEffect } from 'react';
-import { useState } from "react"
-import Card from '../components/cards/Card';
-import instance from '../config/instance';
+import React, { useEffect } from "react";
+import { useState } from "react";
+import Card from "../components/cards/Card";
+import instance from "../config/instance";
 
 export default function Pokemon() {
-  const [pokemons, setPokemons] = useState([])
-  const [allPokemons, setAllPokemons] = useState([])
-  const trainerId = "ce771e34-c3f6-4794-b4cf-167b53144561"
-  const [pokemonsChanged, setPokemonsChanged] = useState(false)
+  const [pokemons, setPokemons] = useState([]);
+  const [pokemonToBeCreated, setPokemonToBeCreated] = useState("");
+  const [allPokemons, setAllPokemons] = useState([]);
+  const trainerId = "499cb299-2848-44b2-b30b-0f92501730ee";
+  const [pokemonsChanged, setPokemonsChanged] = useState(false);
 
-  const postData = []
+  const postData = [];
 
   async function handleSubmit() {
-    const request = await instance.post(`/trainers/${trainerId}/pokemons`, postData)
+    const request = await instance.post(
+      `/trainers/${trainerId}/pokemons`,
+      postData
+    );
 
     if (request.status === 201) {
-      console.log("changed");
-      setPokemonsChanged(!pokemonsChanged)
+      setPokemonsChanged(!pokemonsChanged);
     }
+  }
+
+  async function createPokemon(data) {
+    await instance.post("/pokemons", {
+      name: data,
+    });
+
+    setPokemonsChanged(!pokemonsChanged);
+    setPokemonToBeCreated("");
+  }
+
+  async function handleDelete(id) {
+    await instance.delete(`/trainers/${trainerId}/pokemons/${id}`);
+    setPokemonsChanged(!pokemonsChanged);
   }
 
   useEffect(() => {
     async function getPokemons() {
-      const response = await instance.get(`/trainers/${trainerId}/pokemons`)
-      setPokemons(response.data)
+      const response = await instance.get(`/trainers/${trainerId}/pokemons`);
+      setPokemons(response.data);
     }
 
-    getPokemons()
-  }, [pokemonsChanged])
+    getPokemons();
+  }, [pokemonsChanged]);
 
   useEffect(() => {
     async function getAllPokemons() {
-      const response = await instance.get('/pokemons')
-      setAllPokemons(response.data)
+      const response = await instance.get("/pokemons");
+      setAllPokemons(response.data);
     }
 
-    getAllPokemons()
-  }, [])
+    getAllPokemons();
+  }, []);
+
+  pokemons.map((pokemon) => {
+    console.log(pokemon);
+  });
 
   return (
-    <div className='flex items-center mx-auto my-4'>
-      <div className='mx-auto'>
-        <Card items={pokemons} trainerId={trainerId} />
-        <div className='mx-auto my-16 w-72 h-52 border-4 border-grey-50 p-4 gap-2'>
-          <label htmlFor="pokemon" className='text-indigo-500'>Pokemons: </label>
-          <select name="pokemons" id="pokemon" defaultValue="empty" className='appearence-none border-2 border-indigo-500 w-full' onChange={(e) => {postData.push({ pokemonId: e.target.value })}}>
-            <option disabled value="empty">Nenhum pokemon selecionado.</option>
-            {
-              allPokemons.map((pokemon) => <option key={pokemon.id} value={pokemon.id}>{pokemon.name}</option>)
-            }
-          </select>
-          <button className='bg-indigo-500 p-2 mr-auto text-white' type='submit' onClick={handleSubmit}>Cadastrar</button>
+    <div className="flex items-center mx-auto my-4">
+      <div className="mx-auto">
+        <div className="flex flex-col gap-2 items-center">
+          <div className="flex flex-wrap gap-2 mx-auto">
+            {pokemons.map((pokemon, index) => (
+              <Card item={pokemon} handleDelete={handleDelete} key={index} />
+            ))}
+          </div>
+          <div className="flex">
+            <div className="my-16 w-72 h-52 border-4 border-grey-50 p-4 gap-2">
+              <label htmlFor="pokemon" className="text-indigo-500">
+                Cadastre um novo pokemon:{" "}
+              </label>
+              <input
+                type="text"
+                className="appearence-none border-2 border-indigo-500 w-full"
+                onChange={(e) => {
+                  setPokemonToBeCreated(e.target.value);
+                  console.log(e.target.value);
+                }}
+              />
+              <button
+                className="bg-indigo-500 p-2 mr-auto text-white"
+                type="submit"
+                onClick={() => createPokemon(pokemonToBeCreated)}
+              >
+                Cadastrar
+              </button>
+            </div>
+            <div className="my-16 w-72 h-52 border-4 border-grey-50 p-4 gap-2">
+              <label htmlFor="pokemon" className="text-indigo-500">
+                Cadastre um novo pokemon para esse usuário:{" "}
+              </label>
+              <select
+                name="pokemons"
+                id="pokemon"
+                defaultValue="empty"
+                className="appearence-none border-2 border-indigo-500 w-full"
+                onChange={(e) => {
+                  postData.push({ pokemonId: e.target.value });
+                }}
+              >
+                <option disabled value="empty">
+                  Nenhum pokemon selecionado.
+                </option>
+                {allPokemons.map((pokemon) => (
+                  <option key={pokemon.id} value={pokemon.id}>
+                    {pokemon.name}
+                  </option>
+                ))}
+              </select>
+              <button
+                className="bg-indigo-500 p-2 mr-auto text-white"
+                type="submit"
+                onClick={handleSubmit}
+              >
+                Cadastrar
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
-  )
-} 
+  );
+}
