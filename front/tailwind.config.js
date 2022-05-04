06be59a2024eda14326d5a580814539f239fc44b
fabiorodrigolimamartins <fabiorodrigolima@gmail.com>
2022-05-04 08:45:09 -0300

    Adicionada a biblioteca react dom para lidar com o roteamento do projeto.

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -6,7 +6,14 @@ module.exports = {
         'delete-icon': "./src/icons/crud/delete.svg"
       },
       backgroundColor: {
-        "not-so-grey": "#F8F8F8"
+        "not-so-grey": "#F8F8F8",
+        "theme": "#5AAD9F"
+      },
+      borderColor: {
+        "theme": "#5AAD9F"  
+      },
+      textColor: {
+        "theme": "#5AAD9F"
       }
     },
   },
