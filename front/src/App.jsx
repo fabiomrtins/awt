e183c6cd32533cddb0ade353864a15b2b61ec522
Fábio Martins <fabio.martins@tascominformatica.com.br>
2022-07-05 22:47:30 -0300

    Modificação no app para registro de um usuário

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,21 +1,29 @@
-import { useState } from "react";
+import { useEffect } from "react";
 import Pokemon from "./pages/Pokemon";
 import Type from "./pages/Type";
 import bulbasaur from "./icons/pokemons/bulba.svg";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-// import logo from './logo.svg'
 import "./App.css";
-
-localStorage.setItem("trainerId", "63b2dddf-c2d7-497a-8fd1-e236990b16d0");
+import instance from "./config/instance";
 
 function App() {
+  useEffect(async () => {
+    await instance.post('/trainers', {
+      "id": "63b2dddf-c2d7-497a-8fd1-e236990b16d0",
+      "name": "user",
+      "email": "user@mail.com",
+      "password": "pass"
+    })
+  }, [])
+  
+  localStorage.setItem("trainerId", "63b2dddf-c2d7-497a-8fd1-e236990b16d0");
+
   return (
     <div className="App bg-white">
       <Router>
         <div className="flex gap-2 items-center w-full h-20 bg-theme">
           <img className="w-16 h-auto" src={bulbasaur} />
           <Link to="/" className="text-white font-semibold text-4xl">Bulba</Link>
-          {/* <h1 className="text-white font-semibold text-4xl">Bulba</h1> */}
           <div className="flex gap-2 items-center text-white">
             <Link to="/">Pokemons</Link>
             |
